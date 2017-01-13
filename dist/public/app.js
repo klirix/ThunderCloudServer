30d61686f827677d8a745a68c18be80d4937455e
Felix Ligov <you@example.com>
2017-01-13 22:19:06 +0300

    Stylized app a bit with a snackbar.js

--- dist/public/app.js
+++ dist/public/app.js
@@ -5,7 +5,11 @@ var app = new Vue({
     files: []
   },
   methods: {
-    deleteFile: function(e,id){
+    deleteFile: function(e,id,name){
+        $.snackbar({
+            content: "File "+name+" successfully deleted!",// add a custom class to your snackbar
+            timeout: 3000, // time in milliseconds after the snackbar autohides, 0 is disabled
+        })
         e.preventDefault();
         $.ajax('file/'+id,{
             method:'DELETE'
@@ -32,4 +36,17 @@ socket.on('deleted',function(data){
 })
 
 
-new Dropzone(".drop", { url: "/files"})
+var big = new Dropzone(".drop", { url: "/files"})
+var plus = new Dropzone(".add-file", { url: "/files"})
+
+
+big.on('addedfile',addedFile)
+plus.on('addedfile',addedFile)
+
+function addedFile(file){
+    console.log(file)
+    $.snackbar({
+        content: "File "+file.name+" successfully loaded!",// add a custom class to your snackbar
+        timeout: 3000, // time in milliseconds after the snackbar autohides, 0 is disabled
+    })
+}
