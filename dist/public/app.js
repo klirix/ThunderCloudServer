f7be93dd4c6b749892c3b7988d269ca6e1afc60c
Felix Ligov <you@example.com>
2017-01-17 23:03:11 -0500

    Some front-end changes

--- dist/public/app.js
+++ dist/public/app.js
@@ -4,9 +4,28 @@ var app = new Vue({
   data: {
     contextMenuOpen: false,
     files: [],
-    fileContext: undefined
+    fileContext: undefined,
+    loadingQueue:[]
   },
   methods: {
+    dragstart:function(e){
+        e.dataTransfer.dropEffect = 'copy';
+        e.preventDefault();
+        
+    },
+    dragstop:function(e){
+        console.log(e);
+    },
+    drop:function(e){
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'copy';
+        console.log(e.dataTransfer.files);
+        for (var i in e.dataTransfer.files) {
+            if (e.dataTransfer.files.hasOwnProperty(i)) {
+                this.uploadFile(e.dataTransfer.files[i])
+            }
+        }
+    },
     openContextMenu: function(e,file){
         this.contextMenuOpen = true;
         this.fileContext = file;
@@ -22,6 +41,36 @@ var app = new Vue({
             $(".backdrop").removeClass('menu-open');
         }
         console.log(e);
+    },
+    uploadFile:function(file){
+        var form = new FormData();
+        form.append('file',file);
+        // var id = this.loadingQueue.length;
+        // this.loadingQueue.push(0)
+        $.ajax({
+            url: '/files',
+            type: 'POST',
+            cache: false,
+            xhr: function(){
+                var xhr = new window.XMLHttpRequest();
+                xhr.addEventListener("progress", function (evt) {
+                    if (evt.lengthComputable) {
+                        var percentComplete = evt.loaded / evt.total;
+                        // this.loadingQueue[id-1] = percentComplete;
+                    }
+                }, false);
+                return xhr;
+            },
+            data:form,
+            processData: false, // Don't process the files
+            contentType: false
+        }).then(function(){
+            
+            $.snackbar({
+                content: "File "+file.original+" successfully deleted!",// add a custom class to your snackbar
+                timeout: 3000, // time in milliseconds after the snackbar autohides, 0 is disabled
+            })
+        })
     },
     downloadFile:function(e,file){
         console.log(e);
@@ -105,7 +154,7 @@ function addedFile(file){
 function getThumbnail(file){
     var format = file.original.lastIndexOf('.')+1;
     if(['jpg','png'].indexOf(format)>-1){
-        return 'file/'+file.id;
+        return '/file/'+file.id;
     }
     return '/pics/'+format+'-file-format.svg'
 }
